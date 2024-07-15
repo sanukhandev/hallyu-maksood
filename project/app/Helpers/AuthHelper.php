e80b571e3fbd821ad7ef604ffdd2cbdd3f80b839
Sanu Khan-Business Partner <sanu.khan-external@alfuttaim.com>
2024-07-15 13:18:16 +0400

    Debug 3

--- project/app/Helpers/AuthHelper.php
+++ project/app/Helpers/AuthHelper.php
@@ -4,32 +4,23 @@ namespace App\Helpers;
 
 use App\Models\Socialsetting;
 
-use Config;
 use Google_Client;
 use Google_Service_Oauth2;
 class AuthHelper
 {
     private Google_Client $googleClient;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->socialSettings = Socialsetting::findOrFail(1);
         $this->googleClient = new Google_Client();
-
-        Config::set('services.google.client_id', $this->socialSettings->google_client_id);
-        Config::set('services.google.client_secret', $this->socialSettings->google_client_secret);
-        Config::set('services.google.redirect', $this->socialSettings->google_redirect);
-
-        $this->googleClient->setClientId(config('services.google.client_id'));
-        $this->googleClient->setClientSecret(config('services.google.client_secret'));
-        $this->googleClient->setRedirectUri(config('services.google.redirect'));
-        $this->googleClient = new Google_Client(['client_id' => config('services.google.client_id')]);
-
+        $this->googleClient->setClientId($this->socialSettings->gclient_id);
+        $this->googleClient->setClientSecret($this->socialSettings->gclient_secret);
+        $this->googleClient->setRedirectUri($this->socialSettings->gredirect);
     }
 
-    public function verifyGoogleToken($token){
-        // show config
-         dd($this->socialSettings);
 
+    public function verifyGoogleToken($token){
         $this->googleClient->setAccessToken($token);
         if ($this->googleClient->isAccessTokenExpired()) {
             if ($this->googleClient->getRefreshToken()) {
