a2536ab2a93de433f447e464c7bf020fdf0bf328
Sanu Khan <sanulgbello@gmail.com>
2024-07-16 19:40:41 +0400

    user detials corrected

--- project/app/Http/Controllers/Api/UserController.php
+++ project/app/Http/Controllers/Api/UserController.php
@@ -4,14 +4,15 @@ namespace app\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use http\Client\Request;
 
 class UserController extends Controller
 {
    // get auth user info
 
-    public function getUserInfo()
+    public function getUserInfo(Request $request)
     {
-        $user =  User::with(['orders','wishlists'])->find(auth()->user());
+        $user =  User::with(['orders','wishlists'])->find($request->user()->id);
        if (!$user) {
               return response()->json([
                 'status' => 404,
