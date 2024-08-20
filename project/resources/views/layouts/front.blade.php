3e8c425cb0b1c4a9966c7105a5dff08aa6b4afc7
Sanu Khan <sanulgbello@gmail.com>
2024-08-21 01:15:41 +0400

    new font cairo set

--- project/resources/views/layouts/front.blade.php
+++ project/resources/views/layouts/front.blade.php
@@ -61,7 +61,11 @@
     <link rel="stylesheet" href="{{ asset('assets/front/css/style.css') }}">
      <link rel="stylesheet" href="{{ asset('assets/front/css/category/default.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/front/css/toastr.min.css') }}">
-    @if ($default_font->font_family)
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
+
+@if ($default_font->font_family)
 			<link rel="stylesheet" id="colorr" href="{{ asset('assets/front/css/font.php?font_familly='.$default_font->font_family) }}">
 	@else
 			<link rel="stylesheet" id="colorr" href="{{ asset('assets/front/css/font.php?font_familly='."Open Sans") }}">
